27c6f4606dca2bb4d14cd36ee6e635d814725e06
easyhoo242 <88233704+easyhoo242@users.noreply.github.com>
2023-05-24 12:42:18 +0800

    Initial commit

---
+++ App.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { ref, onMounted, onUnmounted } from 'vue';
+// Implement ...
+function useEventListener(target, event, callback) {
+  onMounted(() => {
+    target?.addEventListener(event, callback);
+  });
+
+  onUnmounted(() => {
+    target?.removeEventListener(event, callback);
+  });
+}
+
+// Implement ...
+function useMouse() {
+  const x = ref(0);
+  const y = ref(0);
+
+  useEventListener(window, 'mousemove', (e) => {
+    x.value = e.clientX;
+    y.value = e.clientY;
+  });
+
+  return {
+    x,
+    y,
+  };
+}
+const { x, y } = useMouse();
+</script>
+
+<template>Mouse position is at: {{ x }}, {{ y }}</template>
---
+++ README.md
@@ -0,0 +1,3 @@
+# skkirx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/skkirx)
